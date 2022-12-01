304def65af86af569f700ac8958954032f5030a4
SirDabao <117444802+SirDabao@users.noreply.github.com>
2022-12-01 02:21:08 +0200

    Complete up to assignment 12(inclusive)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+let str_arr=[...str];
+let arr_letters=str_arr.filter((item)=>(item!==" " && item!=="," && item!=="." && item!=="!" && item!=="?"))
+.map(item=>item.toLowerCase());
+for(let i=0;i<arr_letters.length;i++)
+{
+    if(i>=arr_letters.length-1-i)
+    break;
+    if(arr_letters[i]!==arr_letters[arr_letters.length-1-i])
+    return false;
+}
+return true;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+let current_year=(new Date()).getFullYear();
+return arr.reduce((curr,item)=>
+{
+if ((curr.yearOfDeath || current_year)-curr.yearOfBirth<(item.yearOfDeath || current_year)-item.yearOfBirth)
+{
+curr=item;
+}
+return curr;
+}
+,{yearOfBirth: current_year, yearOfDeath: current_year});
 };
 
 // Do not edit below this line
